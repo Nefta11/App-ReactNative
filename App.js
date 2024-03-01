01deb6aa77b341c4d295d5da0e1528427ac17e1f
Nefta11 <neftaliarturohernandez@gmail.com>
2024-02-29 21:58:37 -0600

    add

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo</Text>
+      <Text>Hola Mundo Siuu</Text>
       <StatusBar style="auto" />
     </View>
   );
